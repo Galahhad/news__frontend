7114bcb7b62eb014729096113bcb5cb34d74aa5e
Suleiman <80757260+Sa1tama54@users.noreply.github.com>
2022-05-12 01:15:03 +0300

    внес правки в названия функций

--- src/getCategories.js
+++ src/getCategories.js
@@ -1,7 +1,7 @@
 import { initialState } from "./initialState";
 import { renderCats } from "./render/categoriesRender";
 
-export const getNewsByCat = () => {
+export const getCategories = () => {
     fetch("http://localhost:3000/categories")
     .then((res) => res.json())
     .then((cats) => {
